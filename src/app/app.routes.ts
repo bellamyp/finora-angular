63ce4434b6b33c98f7fdd75f547b1d97097ff21b
BellamyPhan <bellamyphan@icloud.com>
2025-10-27 21:13:01 -0500

    Guarding home page, user must login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,10 @@
 import { Routes } from '@angular/router';
+import { authGuard } from './guards/auth-guard';
 import { Login } from './components/login/login';
 import { Home } from './components/home/home';
 
 export const routes: Routes = [
   { path: 'login', component: Login },
-  { path: 'home', component: Home },
+  { path: 'home', component: Home , canActivate: [authGuard] },
   { path: '', redirectTo: '/login', pathMatch: 'full' }
 ];
