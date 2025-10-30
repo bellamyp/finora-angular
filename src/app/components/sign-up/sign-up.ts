34a0ba4e3457806af5b95dda6bffc935f3f11c80
BellamyPhan <bellamyphan@icloud.com>
2025-10-30 18:38:48 -0500

    Create Signup page component

---
+++ src/app/components/sign-up/sign-up.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-sign-up',
+  imports: [FormsModule, RouterLink],
+  templateUrl: './sign-up.html',
+  styleUrl: './sign-up.scss',
+})
+export class SignUp {
+
+}
