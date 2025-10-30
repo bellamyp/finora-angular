1c59a33fb78b12d755ffe08ece896a451b62b4cf
BellamyPhan <bellamyphan@icloud.com>
2025-10-30 18:38:06 -0500

    App routing links update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,17 @@ import { Routes } from '@angular/router';
 import { authGuard } from './guards/auth-guard';
 import { Login } from './components/login/login';
 import { Home } from './components/home/home';
+import { SignUp } from './components/sign-up/sign-up';
 
 export const routes: Routes = [
+
+  // Public routes
   { path: 'login', component: Login },
-  { path: 'home', component: Home , canActivate: [authGuard] },
-  { path: '', redirectTo: '/login', pathMatch: 'full' }
+  { path: 'signup', component: SignUp },
+
+  // Protected routes
+  { path: 'home', component: Home, canActivate: [authGuard] },
+
+  // Wildcard: redirect unmatched routes to login
+  { path: '**', redirectTo: '/login', pathMatch: 'full' }
 ];
