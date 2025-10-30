85a317b66905599321927c4ea0b47e913e16fa70
bellamyphan <bellamyphan@icloud.com>
2025-10-29 23:20:18 -0500

    MainApp: add a UI to ping and check BE status in the header

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PingService } from './services/ping.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
-export class App {
-  protected title = 'finora-angular';
+export class App implements OnInit{
+  title = 'finora-angular';
+  backendStatus: 'loading' | 'up' | 'down' = 'loading';
+
+  constructor(private pingService: PingService) {}
+
+  ngOnInit() {
+    this.checkBackend();
+    // optional: keep checking every 15 seconds
+    setInterval(() => this.checkBackend(), 15000);
+  }
+
+  checkBackend() {
+    this.backendStatus = 'loading';
+    this.pingService.ping().subscribe(result => {
+      this.backendStatus = result === 'pong' ? 'up' : 'down';
+    });
+  }
 }
