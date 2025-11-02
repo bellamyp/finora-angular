8633f781e4858c8c9c0ba2e60e90ae136aa78d45
bellamyphan <bellamyphan@icloud.com>
2025-11-02 11:19:41 -0600

    User Dto will now include role

--- src/app/dto/user.dto.ts
+++ src/app/dto/user.dto.ts
@@ -3,6 +3,6 @@ export interface UserDTO {
   id: number;
   name: string;      // changed from username
   email: string;
+  role: string; // just the role name, e.g., "ROLE_ADMIN"
   // do NOT include password in DTO for security
-  // optional: include role if needed
 }
