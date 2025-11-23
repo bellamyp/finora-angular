3222eb074eaed300d2bfd22bd691fc493e9f79b8
BellamyPhan <bellamyphan@icloud.com>
2025-11-23 01:39:49 -0600

    brand-create component to create a new brand

---
+++ src/app/components/brand-create/brand-create.ts
@@ -0,0 +1,49 @@
+import { Component } from '@angular/core';
+import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+import {Router} from '@angular/router';
+import {BrandService} from '../../services/brand.service';
+import {BrandCreateDto} from '../../dto/brand-create.dto';
+import {CommonModule} from '@angular/common';
+
+@Component({
+  selector: 'app-brand-create',
+  imports: [
+    ReactiveFormsModule, CommonModule
+  ],
+  templateUrl: './brand-create.html',
+  styleUrl: './brand-create.scss',
+})
+export class BrandCreate {
+
+  brandForm!: FormGroup;
+
+  constructor(
+    private fb: FormBuilder,
+    private brandService: BrandService,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.brandForm = this.fb.group({
+      name: ['', Validators.required],
+      location: ['']
+    });
+  }
+
+  submit() {
+    if (this.brandForm.invalid) {
+      this.brandForm.markAllAsTouched();
+      return;
+    }
+
+    const payload: BrandCreateDto = this.brandForm.value;
+
+    this.brandService.createBrand(payload).subscribe({
+      next: () => {
+        alert('Brand created successfully!');
+        this.router.navigate(['/transactions-create']);
+      },
+      error: () => alert('Failed to create brand.')
+    });
+  }
+}
