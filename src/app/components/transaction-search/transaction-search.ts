db3b76f98cff9c22a96c3aa015f769744977e7ca
BellamyPhan <bellamyphan@icloud.com>
2025-11-24 14:44:13 -0600

    transactionSearch > add basic search fields

--- src/app/components/transaction-search/transaction-search.ts
+++ src/app/components/transaction-search/transaction-search.ts
@@ -1,11 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, ReactiveFormsModule} from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-transaction-search',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './transaction-search.html',
   styleUrl: './transaction-search.scss',
 })
-export class TransactionSearch {
+export class TransactionSearch implements OnInit {
 
+  searchForm!: FormGroup;
+  results: any[] = [];
+
+  constructor(
+    private fb: FormBuilder,
+    private http: HttpClient,
+  ) {}
+
+  ngOnInit(): void {
+    this.searchForm = this.fb.group({
+      startDate: [''],
+      endDate: [''],
+      minAmount: [''],
+      maxAmount: [''],
+      bankId: [''],
+      brandId: [''],
+      typeId: [''],
+      keyword: [''],
+    });
+  }
+
+  onSearch() {
+    const criteria = this.searchForm.value;
+
+    this.http.post<any[]>('/api/transactions/search', criteria)
+      .subscribe(res => {
+        this.results = res;
+      });
+  }
 }
