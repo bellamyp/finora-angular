203a643a4f51df27e44b889f7412c2e5e815156b
BellamyPhan <bellamyphan@icloud.com>
2025-11-24 21:25:49 -0600

    BankView > can get the full bank details from the BE

--- src/app/components/bank-view/bank-view.ts
+++ src/app/components/bank-view/bank-view.ts
@@ -1,11 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { BankService } from '../../services/bank.service';
+import { BankDto } from '../../dto/bank.dto';
+import {CommonModule} from '@angular/common';
 
 @Component({
   selector: 'app-bank-view',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './bank-view.html',
   styleUrl: './bank-view.scss',
 })
-export class BankView {
+export class BankView implements OnInit {
 
+  bank?: BankDto;
+  loading = true;
+  error?: string;
+
+  constructor(
+    private route: ActivatedRoute,
+    private bankService: BankService
+  ) {}
+
+  ngOnInit(): void {
+    const bankId = this.route.snapshot.paramMap.get('bankId');
+    if (bankId) {
+      this.bankService.getBankById(bankId).subscribe({
+        next: (data) => {
+          this.bank = data;
+          this.loading = false;
+        },
+        error: (err) => {
+          console.error('Failed to load bank:', err);
+          this.error = 'Failed to load bank details.';
+          this.loading = false;
+        }
+      });
+    } else {
+      this.error = 'Bank ID not found in route.';
+      this.loading = false;
+    }
+  }
 }
