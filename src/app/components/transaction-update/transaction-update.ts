21a28a7b0bc8c986ff95600f28b152c9856b5fba
BellamyPhan <bellamyphan@icloud.com>
2025-11-26 18:31:21 -0600

    TransactionUpdate > newTransaction > Default to today

--- src/app/components/transaction-update/transaction-update.ts
+++ src/app/components/transaction-update/transaction-update.ts
@@ -91,9 +91,16 @@ export class TransactionUpdate implements OnInit {
   }
 
   addTransaction() {
+    // Default to today
+    const today = new Date();
+    const yyyy = today.getFullYear();
+    const mm = String(today.getMonth() + 1).padStart(2, '0');
+    const dd = String(today.getDate()).padStart(2, '0');
+    const todayStr = `${yyyy}-${mm}-${dd}`;
+
     this.transactions.push({
       id: '',
-      date: '',
+      date: todayStr,
       amount: 0,
       notes: '',
       bankId: '',
