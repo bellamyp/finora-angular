558c4d44bb80d8e48b47aae07a5cf8c338dac717
BellamyPhan <bellamyphan@icloud.com>
2025-11-26 18:40:50 -0600

    Initiate locationCreate component

---
+++ src/app/components/location-create/location-create.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-location-create',
+  imports: [],
+  templateUrl: './location-create.html',
+  styleUrl: './location-create.scss',
+})
+export class LocationCreate {
+
+}
