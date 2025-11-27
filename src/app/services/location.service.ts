dc23a6f996306558dce9dbaead1a95bd6dae7ab5
BellamyPhan <bellamyphan@icloud.com>
2025-11-26 18:41:13 -0600

    LocationService now can call BE's api to create or fetch all locations

--- src/app/services/location.service.ts
+++ src/app/services/location.service.ts
@@ -1,8 +1,30 @@
+// location.service.ts
 import { Injectable } from '@angular/core';
+import { BackendConfig } from '../config/backend-config';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { LocationDto } from '../dto/location.dto';
+import { LocationCreateDto } from '../dto/location-create.dto';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LocationService {
-  
+  private apiUrl = `${BackendConfig.springApiUrl}/locations`;
+
+  constructor(private http: HttpClient) {}
+
+  /**
+   * Get all locations
+   */
+  getLocations(): Observable<LocationDto[]> {
+    return this.http.get<LocationDto[]>(this.apiUrl);
+  }
+
+  /**
+   * Create a new location
+   */
+  createLocation(payload: LocationCreateDto): Observable<LocationDto> {
+    return this.http.post<LocationDto>(this.apiUrl, payload);
+  }
 }
