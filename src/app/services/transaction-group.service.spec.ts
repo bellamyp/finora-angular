3cc74ba81b29978f088f4cce7c5ff4bc0c28700f
BellamyPhan <bellamyphan@icloud.com>
2025-11-26 20:12:18 -0600

    Safely remove transactionCreate component

--- src/app/services/transaction-group.service.spec.ts
+++ src/app/services/transaction-group.service.spec.ts
@@ -1,7 +1,6 @@
 import { TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { TransactionGroupService } from './transaction-group.service';
-import { TransactionGroupCreateDto } from '../dto/transaction-group-create.dto';
 import { BackendConfig } from '../config/backend-config';
 
 describe('TransactionGroupService', () => {
